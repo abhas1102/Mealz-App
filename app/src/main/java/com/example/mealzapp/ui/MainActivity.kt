01da000b208c76e5b8e8e2b56f8a741c0240c5e1
Abhas Kumar <abhas1102@gmail.com>
2021-11-18 13:29:46 +0530

    Updated the UI for getting the list in a better manner

---
+++ app/src/main/java/com/example/mealzapp/ui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mealzapp.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.mealzapp.ui.meals.MealsCategoriesScreen
+import com.example.mealzapp.ui.theme.MealzAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MealzAppTheme {
+                // A surface container using the 'background' color from the theme
+                    MealsCategoriesScreen()
+                }
+            }
+        }
+    }
+
+
+
--- app/src/main/java/com/example/model/response/Responses.kt
+++ app/src/main/java/com/example/model/response/Responses.kt
@@ -6,7 +6,7 @@ data class MealsCategoriesResponses( val categories:List<MealResponse>)
 data class MealResponse(
     @SerializedName("idCategory") val id:String,
     @SerializedName("strCategory") val name:String,
-    @SerializedName("strCategoryThumb") val description:String,
-    @SerializedName("strCategoryDescription") val imageUrl:String,
+    @SerializedName("strCategoryDescription") val description:String,
+    @SerializedName("strCategoryThumb") val imageUrl:String,
 
 )
