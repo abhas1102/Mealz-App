6a9b5fa33002701885c5f02585645f8535eef49a
Abhas Kumar <abhas1102@gmail.com>
2021-11-19 17:17:29 +0530

    Prepared and defined navigation graphs for navigating into detail screen

--- app/src/main/java/com/example/mealzapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/mealzapp/ui/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.mealzapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.mealzapp.ui.details.MealDetailScreen
 import com.example.mealzapp.ui.meals.MealsCategoriesScreen
 import com.example.mealzapp.ui.theme.MealzAppTheme
 
@@ -12,11 +19,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             MealzAppTheme {
                 // A surface container using the 'background' color from the theme
-                    MealsCategoriesScreen()
+                    FoodiesApp()
                 }
             }
         }
     }
+
+@Composable
+private fun FoodiesApp(){
+    val navController = rememberNavController()
+    NavHost(navController,startDestination = "destination_meals_list"){
+        composable(route = "destination_meals_list"){
+                MealsCategoriesScreen()
+        }
+        composable(route = "destination_meal_details",
+            arguments = listOf(navArgument("meal_category_id"){
+                type = NavType.StringType
+            })
+        ){
+            MealDetailScreen(meal = )
+        }
+    }
+}
 
 
 
