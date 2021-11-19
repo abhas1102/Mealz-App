7f0e3264ee5f715b3da1d4d5f3fa4dbdff1e4037
Abhas Kumar <abhas1102@gmail.com>
2021-11-19 16:29:17 +0530

    Added meal description column in displayed list

--- app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -5,12 +5,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -43,17 +44,30 @@ fun MealsCategoriesScreen() {
 }
 @Composable
 fun MealCategory(meal:MealResponse){
-    Card(shape = RoundedCornerShape(10.dp), elevation = 2.dp,modifier = Modifier.fillMaxWidth().padding(top = 16.dp)) {
+    Card(shape = RoundedCornerShape(10.dp), elevation = 2.dp,modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 16.dp)) {
         Row{
             //Image
             Image(
                 painter = rememberImagePainter(meal.imageUrl),
                 contentDescription = null,
-                modifier = Modifier.size(88.dp).padding(4.dp)
+                modifier = Modifier
+                    .size(88.dp)
+                    .padding(4.dp)
             )
 
-            Column(modifier = Modifier.align(Alignment.CenterVertically).padding(16.dp)){
+            Column(modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(16.dp)){
                 Text(text = meal.name,style = MaterialTheme.typography.h6)
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(text = meal.description,textAlign = TextAlign.Start, style = MaterialTheme.typography.subtitle2,
+                        overflow = TextOverflow.Ellipsis, maxLines = 10)
+                    
+                }
+
+
             }
 
     }
