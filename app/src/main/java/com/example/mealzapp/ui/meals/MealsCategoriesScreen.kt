e0d2d39665e990b201c2b1fa473fc537b5f07241
Abhas Kumar <abhas1102@gmail.com>
2021-11-19 16:44:17 +0530

    Shrinked the description upto 4 lines and introduced expanding list description on click up arrow

--- app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -1,13 +1,15 @@
 package com.example.mealzapp.ui.meals
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -44,6 +46,7 @@ fun MealsCategoriesScreen() {
 }
 @Composable
 fun MealCategory(meal:MealResponse){
+    var isExpanded by remember{ mutableStateOf(false)}
     Card(shape = RoundedCornerShape(10.dp), elevation = 2.dp,modifier = Modifier
         .fillMaxWidth()
         .padding(top = 16.dp)) {
@@ -55,22 +58,29 @@ fun MealCategory(meal:MealResponse){
                 modifier = Modifier
                     .size(88.dp)
                     .padding(4.dp)
+                    .align(Alignment.CenterVertically)
             )
 
             Column(modifier = Modifier
                 .align(Alignment.CenterVertically)
+                .fillMaxWidth(0.8f)
                 .padding(16.dp)){
                 Text(text = meal.name,style = MaterialTheme.typography.h6)
                 CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                     Text(text = meal.description,textAlign = TextAlign.Start, style = MaterialTheme.typography.subtitle2,
-                        overflow = TextOverflow.Ellipsis, maxLines = 10)
+                        overflow = TextOverflow.Ellipsis, maxLines = if (isExpanded) 10 else 4)
                     
                 }
 
 
             }
+            Icon(imageVector = Icons.Filled.KeyboardArrowUp, contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically).clickable { isExpanded = !isExpanded })
 
     }
+
 
 
     }
